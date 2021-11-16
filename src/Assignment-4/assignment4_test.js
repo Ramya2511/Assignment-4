ef020d302bba9c6d60fbe1bf52ea541aa7bc863e
RAMYA <kandagatlaramya5@gmail.com>
2021-11-16 20:33:34 +0530

    Commit assign-4

--- src/Assignment-4/assignment4_test.js
+++ src/Assignment-4/assignment4_test.js
@@ -14,14 +14,14 @@
   var quixote = require("../../vendor/quixote.js");
 
   // Define a test suite using Mocha's standard `define` function.
-  describe("Assignment-4", function () {
+  describe.only("Assignment-4", function () {
     // Variables used by our tests. They're populated in the `before()` and `beforeEach()` functions.
     var frame; // The Quixote test frame.
     var container, frameDom; // The figure element inside the media object. (The icon.)
     var header_ele, form_ele, input; // The content element inside the media object. (The paragraph.)
 
     // Use Mocha's standard `before` function to set up our Quixote test frame before the tests run.
-    before.only(function (done) {
+    before(function (done) {
       // Create the frame and load our stylesheet.
       frame = quixote.createFrame(
         {
